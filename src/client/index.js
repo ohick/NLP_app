71c06bc0b5563cce5dc17773ed321c338b8a1cd5
ohick <oliviahick1@gmail.com>
2020-02-26 14:28:52 +0100

    added header style

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ import { handleSubmit } from './js/handleSubmit';
 import { urlCheck } from './js/urlChecker';
 
 import './styles/base.scss';
+import './styles/header.scss';
 
 export {
     handleSubmit,
